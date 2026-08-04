652454d036c3b6891effc7973db97fd02e87e58b
Greg Stoll <greg@gregstoll.com>
2026-08-03 21:53:55 -0500

    initial commit, getleaders works

---
+++ cron_update_baseballdivisionraces_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd /home/gregstoll/projects/mlb-triple-crown.git > /dev/null
+. .venv/bin/activate
+.venv/bin/python3 getleaders/getleaders.py -u
+cp getleaders/data/NL.json showleaders/data
+cp getleaders/data/AL.json showleaders/data
+popd > /dev/null
---
+++ getleaders/getleaders.py
@@ -0,0 +1,51 @@
+from __future__ import annotations, division
+import datetime
+import json
+import os
+import time
+import sys
+from pathlib import Path
+
+import statsapi
+
+def get_leader_data(league: str, leaderCategory: str, quiet: bool) -> list:
+    leagueId = 103 if league == 'AL' else 104
+    if not quiet:
+        print(f"getting for league_id={leagueId}, leaderCategory={leaderCategory}")
+    # throttle
+    time.sleep(0.3)
+    leaders : list = statsapi.league_leader_data(leaderCategories=leaderCategory, leagueId=leagueId, gameTypes='R', statGroup='hitting', statType='season', limit=5)
+    return leaders
+
+class LeagueLeaders:
+    def __init__(self, leagueName: str, quiet: bool):
+        self.leagueName = leagueName
+        self.quiet = quiet
+        self.leaders = {'homeRuns': [], 'battingAverage': [], 'runsBattedIn': []}
+
+    def update(self):
+        for cat in ['homeRuns', 'battingAverage', 'runsBattedIn']:
+            leaders : list = get_leader_data(self.leagueName, cat, quiet=True)
+            self.leaders[cat] = leaders
+
+    def get_json_file_path(self) -> Path:
+        data_path = Path(os.path.realpath(__file__)).parent / "data"
+        return data_path / f"{self.leagueName}.json"
+
+    def write_to_json(self):
+        file_path = self.get_json_file_path()
+        os.makedirs(file_path.parent, exist_ok=True)
+        with open(file_path, 'w') as f:
+            f.write(json.dumps(self.leaders))
+
+if __name__ == '__main__':
+    import pprint
+    pp = pprint.PrettyPrinter(indent=2)
+    #meta : list = statsapi.meta('leagueLeaderTypes')
+    #pp.pprint(meta)
+
+    for league in ['AL', 'NL']:
+        ll = LeagueLeaders(leagueName=league, quiet=True)
+        ll.update()
+        ll.write_to_json()
+        #pp.pprint(ll.leaders)
